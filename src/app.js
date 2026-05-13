5ee2603f598be403e45875fb2acfd4a81909d90f
Ntsibiseng <naleshabana@gmail.com>
2026-05-13 11:49:15 +0200

    Added the API to the search engine

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
+//Function to inject the temperature and city on the interface
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature")
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+//Function to handle the weaher apis
+function searchCity(city){
+    //make api call and update the interface
+    let apiKey = "00b474oa02t238b83db838032a1cf481";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 //Function to handle city displaying
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#weather-city");
+    
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Polokwane");
